c0934cabfddcb82e2b718ef27db63ec13db88686
gvstartsev <gvstartsev@yandex.ru>
2020-09-17 13:29:38 +0500

    Commit 2 catch b = true (false?)

--- src/main/java/com/epam/university/java/core/task001/Task001Impl.java
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -2,65 +2,79 @@ package com.epam.university.java.core.task001;
 
 public class Task001Impl implements Task001 {
 
-    @Override
-    public double addition(String firstNumber, String secondNumber) {
-        if (firstNumber == null || firstNumber.isEmpty() || secondNumber == null || secondNumber.isEmpty())
-            throw new IllegalArgumentException("input parameters are not set");
+    private static boolean checkInput (String firstNumber, String secondNumber) {
+        return firstNumber == null || firstNumber.isEmpty() || secondNumber == null || secondNumber.isEmpty();
+    }
 
+    private static boolean checkConvert (String firstNumber, String secondNumber) {
+        boolean b = true;
         try {
             Double.parseDouble(firstNumber);
             Double.parseDouble(secondNumber);
-        } catch (NumberFormatException e) {
-            System.out.println("can't convert input values to numbers");
+        } catch (Exception e) {
+            b = true;
         }
-        return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+        return b;
+    }
+
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        if (checkInput(firstNumber, secondNumber)) {
+            throw new IllegalArgumentException("input parameters are not set");
+        }
+        else if (!checkConvert(firstNumber, secondNumber)) {
+            throw new IllegalArgumentException("can't convert input values to numbers");
+        }
+        else {
+            return Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+        }
     }
 
     @Override
     public double subtraction(String firstNumber, String secondNumber) {
-        if (firstNumber == null || firstNumber.isEmpty() || secondNumber == null || secondNumber.isEmpty())
+        if (checkInput(firstNumber, secondNumber)) {
             throw new IllegalArgumentException("input parameters are not set");
-
-        try {
-            Double.parseDouble(firstNumber);
-            Double.parseDouble(secondNumber);
-        } catch (NumberFormatException e) {
-            System.out.println("can't convert input values to numbers");
         }
-        return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+        else if (!checkConvert(firstNumber, secondNumber)) {
+            throw new IllegalArgumentException("can't convert input values to numbers");
+        }
+        else {
+            return Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+        }
     }
 
     @Override
     public double multiplication(String firstNumber, String secondNumber) {
-        if (firstNumber == null || firstNumber.isEmpty() || secondNumber == null || secondNumber.isEmpty())
+        if (checkInput(firstNumber, secondNumber)) {
             throw new IllegalArgumentException("input parameters are not set");
-
-        try {
-            Double.parseDouble(firstNumber);
-            Double.parseDouble(secondNumber);
-        } catch (NumberFormatException e) {
-            System.out.println("can't convert input values to numbers");
         }
-        return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+        else if (!checkConvert(firstNumber, secondNumber)) {
+            throw new IllegalArgumentException("can't convert input values to numbers");
+        }
+        else {
+            return Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+        }
     }
 
     @Override
-    public double division(String firstNumber, String secondNumber) throws IllegalArgumentException {
-        if (firstNumber == null || firstNumber.isEmpty() || secondNumber == null || secondNumber.isEmpty())
+    public double division(String firstNumber, String secondNumber) {
+        if (checkInput(firstNumber, secondNumber)) {
             throw new IllegalArgumentException("input parameters are not set");
-
-        try {
-            Double.parseDouble(firstNumber);
-            Double.parseDouble(secondNumber);
-        } catch (NumberFormatException e) {
-            System.out.println("can't convert input values to numbers");
         }
-        if (Double.parseDouble(firstNumber) == 0 && Double.parseDouble(secondNumber) == 0) return Double.NaN;
-        else if (Double.parseDouble(firstNumber) < 0 && Double.parseDouble(secondNumber) == 0)
+        else if (!checkConvert(firstNumber, secondNumber)) {
+            throw new IllegalArgumentException("can't convert input values to numbers");
+        }
+        if (Double.parseDouble(firstNumber) == 0 && Double.parseDouble(secondNumber) == 0) {
+            return Double.NaN;
+        }
+        else if (Double.parseDouble(firstNumber) < 0 && Double.parseDouble(secondNumber) == 0) {
             return Double.NEGATIVE_INFINITY;
-        else if (Double.parseDouble(firstNumber) > 0 && Double.parseDouble(secondNumber) == 0)
+        }
+        else if (Double.parseDouble(firstNumber) > 0 && Double.parseDouble(secondNumber) == 0) {
             return Double.POSITIVE_INFINITY;
-
-        else return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+        }
+        else {
+            return Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+        }
     }
 }
